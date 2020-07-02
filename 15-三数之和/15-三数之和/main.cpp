572a96d40e1b9599168d3b018e438629ba7bd468
HustDsy <379644606@qq.com>
2020-07-02 13:59:30 +0800

    排序+双指针 解决时间限制的问题

--- 15-三数之和/15-三数之和/main.cpp
+++ 15-三数之和/15-三数之和/main.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum_timeLimit(vector<int>& nums) {
         vector<vector<int>>result;
         if(nums.size()<3){
             return {};
@@ -43,11 +43,57 @@ public:
         }
         return result;
     }
+    vector<vector<int>>threeSum(vector<int>& nums){
+        vector<vector<int>>result;
+        int size=(int)nums.size();
+        if(size<3){
+            return {};
+        }
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<size;i++){
+            if(nums[i]>0){
+                return result;
+            }else if(i>0&&nums[i]==nums[i-1]){
+                continue;
+            }else{
+                int l=i+1;
+                int r=size-1;
+                while(l<r){
+                    vector<int>temp;
+                    if(nums[i]+nums[l]+nums[r]==0){
+                        temp.push_back(nums[i]);
+                        temp.push_back(nums[l]);
+                        temp.push_back(nums[r]);
+                        result.push_back(temp);
+                        temp.clear();
+                        while(l<r&&nums[l+1]==nums[l]){
+                            l=l+1;
+                        }
+                        l=l+1;
+                        while(l<r&&nums[r-1]==nums[r]){
+                            r=r-1;
+                        }
+                        r=r-1;
+                    }else if(nums[i]+nums[l]+nums[r]>0){
+                        r--;
+                    }else if(nums[i]+nums[l]+nums[r]<0){
+                        l++;
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+                    
 };
 
+
 int main(int argc, const char * argv[]) {
-    Solution ss;
-    vector<int>test={-1,0,1,2,-1,-4};
-    vector<vector<int>>result=ss.threeSum(test);
+    vector<int>t={-1,0,1,2,-1,-4};
+    vector<vector<int>>res;
+    Solution s;
+    res=s.threeSum(t);
     return 0;
 }
+
