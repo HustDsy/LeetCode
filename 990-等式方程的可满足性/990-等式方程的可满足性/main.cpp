533f8a115f79c5824004ab90bbf37c8cd5fc95b9
HustDsy <379644606@qq.com>
2020-08-02 20:07:35 +0800

    并查集

---
+++ 990-等式方程的可满足性/990-等式方程的可满足性/main.cpp
@@ -0,0 +1,70 @@
+//
+//  main.cpp
+//  990-等式方程的可满足性
+//
+//  Created by 董深育 on 2020/8/2.
+//  Copyright © 2020 董深育. All rights reserved.
+//
+
+//并查集
+#include <iostream>
+#include<vector>
+#include<numeric>
+using namespace std;
+
+class UnionFind {
+private:
+    vector<int> parent;
+
+public:
+    UnionFind() {
+        parent.resize(26);
+        iota(parent.begin(), parent.end(), 0);
+    }
+
+    int find(int index) {
+        if (index == parent[index]) {
+            return index;
+        }
+        parent[index] = find(parent[index]);
+        return parent[index];
+    }
+
+    void unite(int index1, int index2) {
+        parent[find(index1)] = find(index2);
+    }
+};
+
+class Solution {
+public:
+    bool equationsPossible(vector<string>& equations) {
+        UnionFind uf;
+        for (const string& str: equations) {
+            if (str[1] == '=') {
+                int index1 = str[0] - 'a';
+                int index2 = str[3] - 'a';
+                uf.unite(index1, index2);
+            }
+        }
+        for (const string& str: equations) {
+            if (str[1] == '!') {
+                int index1 = str[0] - 'a';
+                int index2 = str[3] - 'a';
+                if (uf.find(index1) == uf.find(index2)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
