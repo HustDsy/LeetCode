c5e1d0023948beea19803327241ba78f54754d5d
HustDsy <379644606@qq.com>
2020-08-11 12:06:49 +0800

    no message

--- 120-三角形最小路径和/120-三角形最小路径和/main.cpp
+++ 120-三角形最小路径和/120-三角形最小路径和/main.cpp
@@ -13,8 +13,6 @@ using namespace std;
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        vector<vector<int>> f(n, vector<int>(n));
         f[0][0] = triangle[0][0];
         for (int i = 1; i < n; ++i) {
             f[i][0] = f[i - 1][0] + triangle[i][0];
