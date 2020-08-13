7f1560a0ffee85a46a1b38bb5e03ba386257537b
HustDsy <379644606@qq.com>
2020-08-13 15:23:50 +0800

    竖式相加

---
+++ 43. 字符串相乘/43. 字符串相乘/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  43. 字符串相乘
+//
+//  Created by 董深育 on 2020/8/13.
+//  Copyright © 2020 董深育. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
