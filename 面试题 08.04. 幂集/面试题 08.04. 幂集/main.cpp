3853548553012f3a3f521067e0d908277405c29d
HustDsy <379644606@qq.com>
2020-08-23 15:00:39 +0800

    补充迭代的方法

--- 面试题 08.04. 幂集/面试题 08.04. 幂集/main.cpp
+++ 面试题 08.04. 幂集/面试题 08.04. 幂集/main.cpp
@@ -11,8 +11,33 @@
 using namespace std;
 
 
-class Solution {
+class Solution{
 public:
+    
+    vector<int>kk;
+    vector<vector<int>>ret;
+    
+    void getRet(int len,int num,vector<int>nums,int begin,int n){
+        if(len==num){
+            ret.push_back(kk);
+            return;
+        }
+        for(int i=begin;i<n;i++){
+            kk.push_back(nums[i]);
+            getRet(len, num+1, nums, i+1,n);
+            kk.pop_back();
+        }
+        
+    }
+    vector<vector<int>> subsets_it(vector<int>& nums) {
+        ret.push_back(vector<int>());
+        int n=static_cast<int>(nums.size());
+        for(int len=1;len<=n;len++){
+             getRet(len, 0, nums, 0,n);
+        }
+    
+        return ret;
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int>kk={};
         vector<vector<int>>ret;
@@ -27,6 +52,8 @@ public:
         }
         return ret;
     }
+    
+    
 };
 
 
