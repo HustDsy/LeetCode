abedd1c928cf6325b395d15c68b3e8240b1c5e58
HustDsy <379644606@qq.com>
2020-09-13 19:23:50 +0800

    no message

--- 79. 单词搜索/79. 单词搜索/main.cpp
+++ 79. 单词搜索/79. 单词搜索/main.cpp
@@ -2,89 +2,76 @@
 //  main.cpp
 //  79. 单词搜索
 //
-//  Created by 董深育 on 2020/8/26.
+//  Created by 董深育 on 2020/9/13.
 //  Copyright © 2020 董深育. All rights reserved.
 //
 
 #include <iostream>
 #include<vector>
-#include<string>
-#include<stack>
 using namespace std;
+
 
 class Solution {
 public:
-    bool flag=false;
-    bool isCan(int x,int y,int col,int row){
-        return x>=0&&x<row&&y>=0&&y<col;
+    inline bool isvaild(int i,int j,int row,int col){
+        return i<row&&j<col&&i>=0&&j>=0;
         
     }
-    bool dfs(int x,int y,vector<vector<char>>& board, string word,int kk,vector<vector<int>>visited,int col,int row){
-        if(kk==(int)word.length()){
-            flag=true;
+    bool dfs(string word,int i,int j,vector<vector<char>>& board,int row,int col,int k){
+        if(k==word.length()){
             return true;
         }
+        //如果上下左右都不相等 返回false
+        
         //上
-        if(isCan(x-1, y, col, row)&&board[x-1][y]==word[kk]&&!visited[x-1][y]){
-            visited[x-1][y]=1;
-            if(dfs(x-1,y,board,word,kk+1,visited,col,row)){
-                return true;
-            }
-            visited[x-1][y]=0;
+        if(isvaild(i-1, j, row, col)&&board[i-1][j]==word[k]){
+            board[i-1][j]='#';//标记已经访问过
+            if(dfs(word,i-1,j,board,row,col,k+1)) return true;
+            board[i-1][j]=word[k];
         }
         //下
-        if(isCan(x+1, y, col, row)&&board[x+1][y]==word[kk]&&!visited[x+1][y]){
-            visited[x+1][y]=1;
-            if(dfs(x+1,y,board,word,kk+1,visited,col,row)){
-                return true;
-            }
-            visited[x+1][y]=0;
+        if(isvaild(i+1, j, row, col)&&board[i+1][j]==word[k]){
+            board[i+1][j]='#';//标记已经访问过
+            if(dfs(word,i+1,j,board,row,col,k+1)) return true;
+            board[i+1][j]=word[k];
         }
         //左
-        if(isCan(x, y-1, col, row)&&board[x][y-1]==word[kk]&&!visited[x][y-1]){
-            visited[x][y-1]=1;
-            if(dfs(x,y-1,board,word,kk+1,visited,col,row)){
-                return true;
-            }
-            visited[x][y-1]=0;
+        if(isvaild(i, j-1, row, col)&&board[i][j-1]==word[k]){
+            board[i][j-1]='#';//标记已经访问过
+            if (dfs(word,i,j-1,board,row,col,k+1)) return true;;
+            board[i][j-1]=word[k];
         }
         //右
-        if(isCan(x, y+1, col, row)&&board[x][y+1]==word[kk]&&!visited[x][y+1]){
-            visited[x][y+1]=1;
-            if (dfs(x,y+1,board,word,kk+1,visited,col,row)){
-                return true;
-            }
-            visited[x][y+1]=0;
+        if(isvaild(i, j+1, row, col)&&board[i][j+1]==word[k]){
+            board[i][j+1]='#';//标记已经访问过
+            if(dfs(word,i,j+1,board,row,col,k+1)) return  true;
+            board[i][j+1]=word[k];
         }
         return false;
     }
+    
     bool exist(vector<vector<char>>& board, string word) {
-        int row=static_cast<int>(board.size());
-        if(row==0){
-            return false;
-        }
-        int col=static_cast<int>(board[0].size());
+        int row=(int)board.size();
+        if(row==0) return false;
+        int col=(int)board[0].size();
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(board[i][j]==word[0]){
-                    int kk=1;
-                    vector<vector<int>>visited(row,vector<int>(col,0));
-                    visited[i][j]=1;
-                    bool tt=dfs(i,j,board,word,kk,visited,col,row);
-                    if(tt){
-                        return true;
-                    }
+                    board[i][j]='#';
+                    if(dfs(word,i,j,board,row,col,1)) return true;
+                    board[i][j]=word[0];
                 }
+                
             }
         }
         return false;
     }
 };
+
+
 int main(int argc, const char * argv[]) {
     Solution s;
-    vector<vector<char> >board={{'A','B','C','E'},
-        {'S','F','C','S'},
-        {'A','D','E','E'}};
-    s.exist(board, "ABCCEd");
+    vector<vector<char> > board={{'a','a'}};
+    cout<<s.exist(board, "aaa");
     return 0;
 }
