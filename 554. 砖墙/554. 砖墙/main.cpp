a728f4327497f293afd4d37d553461a67d0928c6
HustDsy <379644606@qq.com>
2021-05-06 10:45:05 +0800

    no message

--- 554. 砖墙/554. 砖墙/main.cpp
+++ 554. 砖墙/554. 砖墙/main.cpp
@@ -6,6 +6,28 @@
 //
 
 #include <iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int leastBricks(vector<vector<int>>& wall) {
+
+        unordered_map<int,int>total;
+        int maxgap=0;
+        for(vector<int>w:wall){
+            int sum=0;
+            for(int i=0;i<w.size()-1;i++){
+                int cur=w[i];
+                sum+=cur;
+                total[sum]++;
+                maxgap=max(total[sum],maxgap);
+            }
+        }
+        return wall.size()-maxgap;
+    }
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
