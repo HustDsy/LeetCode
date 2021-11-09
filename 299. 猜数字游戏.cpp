71b00024d979a83bb9bc3d49609eca6760d50e58
HustDsy <379644606@qq.com>
2021-11-09 10:22:29 +0800

    test for priority

--- 299. 猜数字游戏.cpp
+++ 299. 猜数字游戏.cpp
@@ -4,22 +4,20 @@ using namespace std;
 class Solution {
    public:
     string getHint(string secret, string guess) {
-        unordered_map<char, int> m;
+        vector<int> nums(10, 0);
         int l = secret.length();
         int A = 0, B = 0;
-        vector<int> flag(l, 0);
         for (int i = 0; i < l; i++) {
             if (secret[i] == guess[i]) {
                 A++;
-                flag[i] = 1;
-            } else {
-                m[secret[i]]++;
-            }
-        }
-        for (int i = 0; i < l; i++) {
-            if (m.count(guess[i]) && m[guess[i]] > 0 && !flag[i]) {
-                m[guess[i]]--;
-                B++;
+            } else  {
+                if (nums[guess[i] - '0']-- > 0) {
+                    B++;
+                }
+                
+                if (nums[secret[i] - '0']++ < 0) {
+                    B++;
+                }
             }
         }
         string res = to_string(A) + 'A' + to_string(B) + 'B';
