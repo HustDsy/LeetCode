bcfbe73a63d73f3ba4ca9f35d9d693773427ce66
HustDsy <379644606@qq.com>
2021-11-15 10:55:56 +0800

    dictionary tree

---
+++ 319. 灯泡开关.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // n个灯泡，操作n轮
+    // 对于第k个灯泡，
+    int bulbSwitch(int n) {
+        return sqrt(double(n));
+    }
+};
