2911f52b050c716cc3501b9d82aa8ccd31a38128
HustDsy <379644606@qq.com>
2021-11-24 16:06:24 +0800

    get latest version

--- 859. 亲密字符串.cpp
+++ 859. 亲密字符串.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-public:
-    bool buddyStrings(string s, string goal) {  
+   public:
+    bool buddyStrings(string s, string goal) {
         int ls = s.length();
         int gs = goal.length();
         if (ls != gs) {
             return false;
-        } 
+        }
         bool dou = false;
-        set<char>check;
+        set<char> check;
         for (char c : s) {
             if (check.count(c)) {
                 dou = true;
@@ -22,10 +22,9 @@ public:
         }
         int a = -1, b = -1;
         for (int i = 0; i < ls; i++) {
-            if(s[i] != goal[i]) {
-                 b = a == -1? -1 : a; 
+            if (s[i] != goal[i]) {
+                b = a == -1 ? -1 : a;
                 a = i;
-               
             }
         }
         if (a == -1 || b == -1) {
@@ -33,6 +32,5 @@ public:
         }
         swap(s[a], s[b]);
         return s == goal;
-
     }
 };
